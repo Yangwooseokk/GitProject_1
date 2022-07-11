283a8afd9a1a74d7bde65ff413dacf6e7f67d3fd
dap06044@naver.com <dap06044@naver.com>
2022-07-11 19:07:10 +0900

    commit-1

---
+++ RevertProject/src/com/sist/main/MainClass.java
@@ -0,0 +1,22 @@
+package com.sist.main;
+/*
+ * git log --oneline -n 3
+ * -- (HEAD)
+ * --
+ * --
+ * 
+ * git format-patch-1 (HEAD=> ID)
+ * git revert HEAD
+ */
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Revert Commit -1");
+		System.out.println("Hello Revert Commit -2");
+		System.out.println("Hello Revert Commit -3");
+		System.out.println("Hello Revert Commit -4");
+		
+	}
+
+}
